a8526e5b99116a6d2e9d917c821cc59802b01db0
James Tucker <jimmytucker0@gmail.com>
2024-09-06 13:15:17 -0700

    fix styles and tweak animations

--- js/index.js
+++ js/index.js
@@ -405,16 +405,27 @@ var tl1 = gsap.timeline(
       trigger: "#circleAnimation1",
       //   pin: true, // pin the trigger element while active
       // toggleActions: "play none none pause",
-      start: "top +=200", // when the top of the trigger hits the top of the viewport
-      end: "+=800", // end after scrolling 500px beyond the start
+      start: "top 100px", // when the top of the trigger hits the top of the viewport
+      end: "+=400", // end after scrolling 500px beyond the start
       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     },
   }
 );
-tl1.to("#circleAnimation1", { clipPath: "circle(25vw at 50% 0%)" });
-tl1.to("#circleAnimation1", { clipPath: "circle(50vw at 50% 0%)" });
-tl1.to("#circleAnimation1", { clipPath: "circle(100vw at 50% 300px)" });
-tl1.to("#circleAnimation1", { clipPath: "circle(10000px at 50% 300px)" });
+tl1.to("#circleAnimation1", { clipPath: "ellipse(80vw 50vw at 50% 50vw)" });
+tl1.to("#circleAnimation1", { clipPath: "ellipse(100vw 80vw at 50% 50vw)" });
+tl1.to("#circleAnimation1", { clipPath: "ellipse(100vw 100vw at 50% 50vw)" });
+tl1.to("#circleAnimation1", { clipPath: "ellipse(10000px 10000px at 50% 50vw)" });
+
+// tl1.to("#circleAnimation1", { clipPath: "circle(50vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(75vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(100vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(10000px at 50% 50vw)" });
+
+
+// tl1.to("#circleAnimation1", { clipPath: "circle(25vw at 50% 0%)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(50vw at 50% 0%)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(100vw at 50% 300px)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(10000px at 50% 300px)" });
 
 var tl2 = gsap.timeline(
   /*{repeat: 2, repeatDelay: 1}*/
@@ -423,17 +434,21 @@ var tl2 = gsap.timeline(
     scrollTrigger: {
       trigger: "#circleAnimation2",
       // pin: true, // pin the trigger element while active
-      start: "top +=200", // when the top of the trigger hits the top of the viewport
-      end: "+=800", // end after scrolling 500px beyond the start
+      start: "top +=300px", // when the top of the trigger hits the top of the viewport
+      end: "+=400", // end after scrolling 500px beyond the start
       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     },
   }
 );
 tl2.to("#circleAnimation2", { backgroundColor: "white" });
-tl2.to("#circleAnimation2", { clipPath: "circle(25vw at 50% 0%)" });
-tl2.to("#circleAnimation2", { clipPath: "circle(50vw at 50% 0%)" });
-tl2.to("#circleAnimation2", { clipPath: "circle(100vw at 50% 300px)" });
-tl2.to("#circleAnimation2", { clipPath: "circle(10000px at 50% 300px)" });
+// tl2.to("#circleAnimation2", { clipPath: "circle(50vw at 50% 60vw)" });
+// tl2.to("#circleAnimation2", { clipPath: "circle(75vw at 50% 50vw)" });
+// tl2.to("#circleAnimation2", { clipPath: "circle(100vw at 50% 50vw)" });
+// tl2.to("#circleAnimation2", { clipPath: "circle(10000px at 50% 50vw)" });
+tl1.to("#circleAnimation2", { clipPath: "ellipse(80vw 50vw at 50% 50vw)" });
+tl1.to("#circleAnimation2", { clipPath: "ellipse(100vw 80vw at 50% 50vw)" });
+tl1.to("#circleAnimation2", { clipPath: "ellipse(100vw 100vw at 50% 50vw)" });
+tl1.to("#circleAnimation2", { clipPath: "ellipse(10000px 10000px at 50% 50vw)" });
 
 var tl3 = gsap.timeline(
   /*{repeat: 2, repeatDelay: 1}*/
@@ -442,34 +457,38 @@ var tl3 = gsap.timeline(
     scrollTrigger: {
       trigger: "#circleAnimation3",
       // pin: true, // pin the trigger element while active
-      start: "top +=200", // when the top of the trigger hits the top of the viewport
-      end: "+=800", // end after scrolling 500px beyond the start
+      start: "top +=300px", // when the top of the trigger hits the top of the viewport
+      end: "+=400", // end after scrolling 500px beyond the start
       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     },
   }
 );
-tl3.to("#circleAnimation3", { clipPath: "circle(25vw at 50% 0%)" });
-tl3.to("#circleAnimation3", { clipPath: "circle(50vw at 50% 0%)" });
-tl3.to("#circleAnimation3", { clipPath: "circle(100vw at 50% 300px)" });
-tl3.to("#circleAnimation3", { clipPath: "circle(10000px at 50% 300px)" });
+// tl3.to("#circleAnimation3", { clipPath: "circle(50vw at 50% 60vw)" });
+// tl3.to("#circleAnimation3", { clipPath: "circle(75vw at 50% 50vw)" });
+// tl3.to("#circleAnimation3", { clipPath: "circle(100vw at 50% 50vw)" });
+// tl3.to("#circleAnimation3", { clipPath: "circle(10000px at 50% 50vw)" });
+tl1.to("#circleAnimation3", { clipPath: "ellipse(80vw 50vw at 50% 60vw)" });
+tl1.to("#circleAnimation3", { clipPath: "ellipse(100vw 80vw at 50% 60vw)" });
+tl1.to("#circleAnimation3", { clipPath: "ellipse(100vw 100vw at 50% 60vw)" });
+tl1.to("#circleAnimation3", { clipPath: "ellipse(10000px 10000px at 50% 50vw)" });
 
-var tl4 = gsap.timeline(
-  /*{repeat: 2, repeatDelay: 1}*/
-  {
-    // yes, we can add it to an entire timeline!
-    scrollTrigger: {
-      trigger: "#circleAnimation4",
-      // pin: true, // pin the trigger element while active
-      start: "top +=200", // when the top of the trigger hits the top of the viewport
-      end: "+=800", // end after scrolling 500px beyond the start
-      // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-    },
-  }
-);
-tl4.to("#circleAnimation4", { clipPath: "circle(25vw at 50% 0%)" });
-tl4.to("#circleAnimation4", { clipPath: "circle(50vw at 50% 0%)" });
-tl4.to("#circleAnimation4", { clipPath: "circle(100vw at 50% 300px)" });
-tl4.to("#circleAnimation4", { clipPath: "circle(10000px at 50% 300px)" });
+// var tl4 = gsap.timeline(
+//   /*{repeat: 2, repeatDelay: 1}*/
+//   {
+//     // yes, we can add it to an entire timeline!
+//     scrollTrigger: {
+//       trigger: "#circleAnimation4",
+//       // pin: true, // pin the trigger element while active
+//       start: "top +=200", // when the top of the trigger hits the top of the viewport
+//       end: "+=800", // end after scrolling 500px beyond the start
+//       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+//     },
+//   }
+// );
+// tl4.to("#circleAnimation4", { clipPath: "circle(50vw at 50% 60vw)" });
+// tl4.to("#circleAnimation4", { clipPath: "circle(75vw at 50% 50vw)" });
+// tl4.to("#circleAnimation4", { clipPath: "circle(100vw at 50% 50vw)" });
+// tl4.to("#circleAnimation4", { clipPath: "circle(10000px at 50% 300px)" });
 // // then we can control the whole thing easily...
 // tl.pause();
 // tl.resume();
