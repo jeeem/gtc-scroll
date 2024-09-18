f980e1c020259fb65a66053f02094142e5d04cd7
its-gio <9650600+its-gio@users.noreply.github.com>
2024-09-18 15:49:51 -0700

    Adding new scroll animation for promoters section desktop

---
+++ js/deleted-animation.js
@@ -0,0 +1,110 @@
+// var tl1 = gsap.timeline(
+//   /*{repeat: 2, repeatDelay: 1}*/
+//   {
+//     // yes, we can add it to an entire timeline!
+//     scrollTrigger: {
+//       trigger: "#circleAnimation1",
+//       //   pin: true, // pin the trigger element while active
+//       // toggleActions: "play none none pause",
+//       start: "top bottom", // when the top of the trigger hits the top of the viewport
+//       end: "+=300", // end after scrolling 500px beyond the start
+//       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+//     },
+//   }
+// );
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
+
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(75vw 75vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(150vw 150vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(300vw 300vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(800vw 800vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "ellipse(2000vw 2000vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", {
+//   clipPath: "ellipse(10000vw 10000vw at 50% 50vw)",
+// });
+
+// tl1.to("#circleAnimation1", { clipPath: "circle(50vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(75vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(100vw at 50% 50vw)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(10000px at 50% 50vw)" });
+
+// tl1.to("#circleAnimation1", { clipPath: "circle(25vw at 50% 0%)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(50vw at 50% 0%)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(100vw at 50% 300px)" });
+// tl1.to("#circleAnimation1", { clipPath: "circle(10000px at 50% 300px)" });
+
+// var tl2 = gsap.timeline(
+//   /*{repeat: 2, repeatDelay: 1}*/
+//   {
+//     // yes, we can add it to an entire timeline!
+//     scrollTrigger: {
+//       trigger: "#circleAnimation2",
+//       // pin: true, // pin the trigger element while active
+//       start: "top bottom", // when the top of the trigger hits the top of the viewport
+//       end: "+=300", // end after scrolling 500px beyond the start
+//       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+//     },
+//   }
+// );
+
+// tl2.to("#circleAnimation2", { backgroundColor: "white" });
+
+// tl2.to("#circleAnimation2", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
+// tl2.to("#circleAnimation2", { clipPath: "ellipse(75vw 75vw at 50% 50vw)" });
+// tl2.to("#circleAnimation2", { clipPath: "ellipse(150vw 150vw at 50% 50vw)" });
+// tl2.to("#circleAnimation2", { clipPath: "ellipse(300vw 300vw at 50% 50vw)" });
+// tl2.to("#circleAnimation2", { clipPath: "ellipse(800vw 800vw at 50% 50vw)" });
+// tl2.to("#circleAnimation2", { clipPath: "ellipse(2000vw 2000vw at 50% 50vw)" });
+// tl2.to("#circleAnimation2", {
+//   clipPath: "ellipse(10000vw 10000vw at 50% 50vw)",
+// });
+
+// var tl3 = gsap.timeline(
+//   /*{repeat: 2, repeatDelay: 1}*/
+//   {
+//     // yes, we can add it to an entire timeline!
+//     scrollTrigger: {
+//       trigger: "#circleAnimation3",
+//       // pin: true, // pin the trigger element while active
+//       start: "top bottom", // when the top of the trigger hits the top of the viewport
+//       end: "+=500", // end after scrolling 500px beyond the start
+//       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+//     },
+//   }
+// );
+// tl3.to("#circleAnimation3", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
+// tl3.to("#circleAnimation3", { clipPath: "ellipse(75vw 75vw at 50% 50vw)" });
+// tl3.to("#circleAnimation3", { clipPath: "ellipse(150vw 150vw at 50% 50vw)" });
+// tl3.to("#circleAnimation3", { clipPath: "ellipse(300vw 300vw at 50% 50vw)" });
+// tl3.to("#circleAnimation3", { clipPath: "ellipse(800vw 800vw at 50% 50vw)" });
+// tl3.to("#circleAnimation3", { clipPath: "ellipse(2000vw 2000vw at 50% 50vw)" });
+// tl3.to("#circleAnimation3", {
+//   clipPath: "ellipse(10000vw 10000vw at 50% 50vw)",
+// });
+
+// var tl4 = gsap.timeline(
+//   /*{repeat: 2, repeatDelay: 1}*/
+//   {
+//     // yes, we can add it to an entire timeline!
+//     scrollTrigger: {
+//       trigger: "#circleAnimation4",
+//       // pin: true, // pin the trigger element while active
+//       start: "top +=200", // when the top of the trigger hits the top of the viewport
+//       end: "+=800", // end after scrolling 500px beyond the start
+//       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+//     },
+//   }
+// );
+// tl4.to("#circleAnimation4", { clipPath: "circle(50vw at 50% 60vw)" });
+// tl4.to("#circleAnimation4", { clipPath: "circle(75vw at 50% 50vw)" });
+// tl4.to("#circleAnimation4", { clipPath: "circle(100vw at 50% 50vw)" });
+// tl4.to("#circleAnimation4", { clipPath: "circle(10000px at 50% 300px)" });
+// // then we can control the whole thing easily...
+// tl.pause();
+// tl.resume();
+// tl.seek(1.5);
+// tl.reverse();
--- js/index.js
+++ js/index.js
@@ -391,122 +391,45 @@ const applyAnimation = (grid, animationType) => {
         );
 
       break;
+    case "galleryCenterImageStaggerScroll":
+      // Set some CSS related style values
+      grid.style.setProperty("--grid-width", "100%");
+      grid.style.setProperty("--grid-columns", "3");
+      grid.style.setProperty("--grid-gap", "1rem");
+      const centerItems = [];
+      const outerItems = [];
+      gridItems.forEach((item, i) =>
+        i % 3 === 1 ? centerItems.push(item) : outerItems.push(item)
+      );
+
+      console.log({ centerItems, outerItems });
+
+      timeline
+        .set(gridWrap, {
+          transformStyle: "unset",
+        })
+        .set(centerItems, {
+          y: "-=200",
+        })
+        .set(outerItems, {
+          y: "+=200",
+        })
+        .to(centerItems, {
+          y: "+=200",
+          ease: "power2",
+          z: 500,
+        })
+        .to(outerItems, {
+          y: "-=200",
+          ease: "power2",
+          z: 500,
+        });
+      break;
     default:
       console.error("Unknown animation type.");
       break;
   }
 };
-
-// var tl1 = gsap.timeline(
-//   /*{repeat: 2, repeatDelay: 1}*/
-//   {
-//     // yes, we can add it to an entire timeline!
-//     scrollTrigger: {
-//       trigger: "#circleAnimation1",
-//       //   pin: true, // pin the trigger element while active
-//       // toggleActions: "play none none pause",
-//       start: "top bottom", // when the top of the trigger hits the top of the viewport
-//       end: "+=300", // end after scrolling 500px beyond the start
-//       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-//     },
-//   }
-// );
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
-
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(75vw 75vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(150vw 150vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(300vw 300vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(800vw 800vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "ellipse(2000vw 2000vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", {
-//   clipPath: "ellipse(10000vw 10000vw at 50% 50vw)",
-// });
-
-// tl1.to("#circleAnimation1", { clipPath: "circle(50vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "circle(75vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "circle(100vw at 50% 50vw)" });
-// tl1.to("#circleAnimation1", { clipPath: "circle(10000px at 50% 50vw)" });
-
-// tl1.to("#circleAnimation1", { clipPath: "circle(25vw at 50% 0%)" });
-// tl1.to("#circleAnimation1", { clipPath: "circle(50vw at 50% 0%)" });
-// tl1.to("#circleAnimation1", { clipPath: "circle(100vw at 50% 300px)" });
-// tl1.to("#circleAnimation1", { clipPath: "circle(10000px at 50% 300px)" });
-
-// var tl2 = gsap.timeline(
-//   /*{repeat: 2, repeatDelay: 1}*/
-//   {
-//     // yes, we can add it to an entire timeline!
-//     scrollTrigger: {
-//       trigger: "#circleAnimation2",
-//       // pin: true, // pin the trigger element while active
-//       start: "top bottom", // when the top of the trigger hits the top of the viewport
-//       end: "+=300", // end after scrolling 500px beyond the start
-//       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-//     },
-//   }
-// );
-
-// tl2.to("#circleAnimation2", { backgroundColor: "white" });
-
-// tl2.to("#circleAnimation2", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
-// tl2.to("#circleAnimation2", { clipPath: "ellipse(75vw 75vw at 50% 50vw)" });
-// tl2.to("#circleAnimation2", { clipPath: "ellipse(150vw 150vw at 50% 50vw)" });
-// tl2.to("#circleAnimation2", { clipPath: "ellipse(300vw 300vw at 50% 50vw)" });
-// tl2.to("#circleAnimation2", { clipPath: "ellipse(800vw 800vw at 50% 50vw)" });
-// tl2.to("#circleAnimation2", { clipPath: "ellipse(2000vw 2000vw at 50% 50vw)" });
-// tl2.to("#circleAnimation2", {
-//   clipPath: "ellipse(10000vw 10000vw at 50% 50vw)",
-// });
-
-// var tl3 = gsap.timeline(
-//   /*{repeat: 2, repeatDelay: 1}*/
-//   {
-//     // yes, we can add it to an entire timeline!
-//     scrollTrigger: {
-//       trigger: "#circleAnimation3",
-//       // pin: true, // pin the trigger element while active
-//       start: "top bottom", // when the top of the trigger hits the top of the viewport
-//       end: "+=500", // end after scrolling 500px beyond the start
-//       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-//     },
-//   }
-// );
-// tl3.to("#circleAnimation3", { clipPath: "ellipse(50vw 50vw at 50% 50vw)" });
-// tl3.to("#circleAnimation3", { clipPath: "ellipse(75vw 75vw at 50% 50vw)" });
-// tl3.to("#circleAnimation3", { clipPath: "ellipse(150vw 150vw at 50% 50vw)" });
-// tl3.to("#circleAnimation3", { clipPath: "ellipse(300vw 300vw at 50% 50vw)" });
-// tl3.to("#circleAnimation3", { clipPath: "ellipse(800vw 800vw at 50% 50vw)" });
-// tl3.to("#circleAnimation3", { clipPath: "ellipse(2000vw 2000vw at 50% 50vw)" });
-// tl3.to("#circleAnimation3", {
-//   clipPath: "ellipse(10000vw 10000vw at 50% 50vw)",
-// });
-
-// var tl4 = gsap.timeline(
-//   /*{repeat: 2, repeatDelay: 1}*/
-//   {
-//     // yes, we can add it to an entire timeline!
-//     scrollTrigger: {
-//       trigger: "#circleAnimation4",
-//       // pin: true, // pin the trigger element while active
-//       start: "top +=200", // when the top of the trigger hits the top of the viewport
-//       end: "+=800", // end after scrolling 500px beyond the start
-//       // scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-//     },
-//   }
-// );
-// tl4.to("#circleAnimation4", { clipPath: "circle(50vw at 50% 60vw)" });
-// tl4.to("#circleAnimation4", { clipPath: "circle(75vw at 50% 50vw)" });
-// tl4.to("#circleAnimation4", { clipPath: "circle(100vw at 50% 50vw)" });
-// tl4.to("#circleAnimation4", { clipPath: "circle(10000px at 50% 300px)" });
-// // then we can control the whole thing easily...
-// tl.pause();
-// tl.resume();
-// tl.seek(1.5);
-// tl.reverse();
 
 // Apply animations to each grid
 const scroll = () => {
@@ -515,18 +438,23 @@ const scroll = () => {
     let animationType;
     switch (i % 6) {
       case 0:
+        // Tour section Desktop
         animationType = "galleryScrollLeft";
         break;
       case 1:
+        // Tour section Mobile
         animationType = "scrollUpTwirl";
         break;
       case 2:
-        animationType = "galleryBacktoForward";
+        // Promotions Section
+        animationType = "galleryCenterImageStaggerScroll";
         break;
       case 3:
+        // Venue Section Desktop
         animationType = "galleryScrollRight";
         break;
       case 4:
+        // Venue Section Mobile
         animationType = "scrollUpTwirl";
         break;
       default:
@@ -649,5 +577,5 @@ preloadImages(".grid__item-inner").then(() => {
   setTimeout(venueTextAnimation, 1000);
   setTimeout(() => {
     resizegallerySections();
-  }, 1000)
+  }, 1000);
 });
